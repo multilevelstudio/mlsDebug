652e36ffb2c1b881704ed5a55b2d9783cfaf36a1
Wagner Pereira <wagner@expansiva.com.br>
2019-08-05 12:01:20 -0300

    Initial

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,14 @@ import * as l2Debug from './L2Debug.js';
 var router = express.Router();
 
 router.post('/l2save', function (req: express.Request, res: express.Response) {
+
     try {
+        let origin: string = req.headers.origin ? <string>req.headers.origin : "";
+        if ((origin !== "https://multilevelstudio.com") && (!origin.startsWith("http://localhost"))) {
+            res.status(500).send("error, invalid origin");
+            return;
+        }
+
         let rc: string = l2Debug.apiUpdateTs(
             req.body.fileName,
             req.body.ts,
@@ -12,12 +19,13 @@ router.post('/l2save', function (req: express.Request, res: express.Response) {
             req.body.map
         );
         console.log("return 200: ", rc);
-        res.header("Access-Control-Allow-Origin", "http://localhost:5000");
+        res.header("Access-Control-Allow-Origin", "*");
         res.status(200).send(rc);
     } catch (e) {
         console.log("return 500: " , e);
         res.status(500).send(e);
     }
+
 });
 
 module.exports = router;
